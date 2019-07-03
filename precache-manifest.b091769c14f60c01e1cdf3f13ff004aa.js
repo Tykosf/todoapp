5d7e1404b4ab17694506aff396e96aeec058a775
tykosf <prromanyi@gmail.com>
2019-07-03 20:16:59 +0300

    add files

---
+++ precache-manifest.b091769c14f60c01e1cdf3f13ff004aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc51bbe70243c534515a16f915bfb07a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ac7c4ad07b2d50eb9c4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d76c3fc472bbb2517d6d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6ac7c4ad07b2d50eb9c4",
+    "url": "/static/js/2.d18538c4.chunk.js"
+  },
+  {
+    "revision": "d76c3fc472bbb2517d6d",
+    "url": "/static/js/main.396971b7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
