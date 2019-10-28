92da2a5aad9fbf69b00d2bd9d909c6c1db1847cf
Tykosf <47809412+Tykosf@users.noreply.github.com>
2019-10-28 20:39:58 +0200

    Update TodoInput.jsx

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 const TodoInput = ({  item, handleChange, handleSubmit, editItem }) => {
@@ -29,38 +28,5 @@ const TodoInput = ({  item, handleChange, handleSubmit, editItem }) => {
 			</form>
 		</div>
 	)
-=======
-import React, { Component } from 'react';
-
-class TodoInput extends Component{
-    render(){
-        const { item, handleChange, handleSubmit, editItem} = this.props;
-        return(
-        <div className="card card-body my-3">
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <div className="input-group-prepend">
-                        <div className="input-group-text bg-primary text-white">
-                            <i className="fas fa-tasks" />
-                        </div>
-                    </div>
-                    <input 
-                     type="text" 
-                     className="form-control text-capitalize"
-                     placeholder="add your task"
-                     value={item}
-                     onChange={handleChange}
-                     />
-                </div>
-                <button type="submit"
-                    className={editItem ? "btn btn-block btn-success mt-3 text-capitalize": "btn btn-block btn-primary mt-3 text-capitalize"}>
-                {editItem ? "edit item" : "add item"}
-                </button>
-            </form>
-        </div>
-        )
-    }
->>>>>>> d640257707603c0c674ca783e605d8f228ec7d59
-}
 
 export default TodoInput
